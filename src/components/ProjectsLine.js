97448c360ff14657199a6082dc54b57a4155ad28
Matthew Lu <seedmachinegun@gmail.com>
2022-05-17 13:09:57 +0100

    Updated ProjectLines' text color, and layout for Foodbag. Extracted text content into a file constant/textContent.js

--- src/components/ProjectsLine.js
+++ src/components/ProjectsLine.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import foodieClub from '../img/foodie-club.png'
 import foodbag from '../img/foodbag.gif'
+import { textContent } from '../constants/textContent'
 
 function ProjectsLine() {
   return (
@@ -10,7 +11,7 @@ function ProjectsLine() {
       <div className='absolute w-0 h-full mt-6 border border-slate-400'></div>
       {/* Project Blocks Container */}
       <div className='relative mt-12'>
-        {/* Project Block */}
+        {/* Project Block - Foodie Club */}
         <div className='my-16 flex'>
           <div className='relative w-full md:w-2/3 h-96 shadow-lg'>
             <img
@@ -20,35 +21,29 @@ function ProjectsLine() {
             />
             {/* Text Block in Small Screen */}
             <div className='md:hidden absolute top-0 flex flex-col items-start p-6 w-full h-full bg-slate-900 opacity-90'>
-              <h4 className='text-white text-xl font-semibold my-2'>Foodie Club</h4>
-              <h5 className='text-white text-left italic'>A Platform for matching groups of foodies to dine together</h5>
-              <p className='text-white text-left my-8'>A web app for finding foodies with similar taste and dine together. Create groups by selecting your favorite restaurants and pick a date and time. Or join groups initated by others. Use the group chat to confirm details.</p>
+              <h4 className='text-white text-xl font-semibold my-2'>{textContent.projects.foodieClub.title}</h4>
+              <h5 className='text-sky-500 text-left italic'>{textContent.projects.foodieClub.subtitle}</h5>
+              <p className='text-white text-left my-8'>{textContent.projects.foodieClub.description}</p>
               {/* Tech Stack */}
-              <div className='text-white flex flex-wrap justify-self-end text-xs'>
-                <span className='mx-2'>React</span>
-                <span className='mx-2'>FeathersJS</span>
-                <span className='mx-2'>MaterialUI</span>
-                <span className='mx-2'>PostgreSQL</span>
+              <div className='text-sky-500 flex flex-wrap justify-self-end text-xs'>
+                {textContent.projects.foodieClub.techStacks.map(tech => <span className='mx-2'>{tech}</span>)}
               </div>
             </div>
           </div>
           {/* Text Block in Large Screen */}
           <div className='hidden md:block w-96 p-6'>
-            <h4 className='text-left text-xl font-semibold my-2'>Foodie Club</h4>
-            <h5 className='text-left italic'>A Platform for matching groups of foodies to dine together</h5>
-            <p className='text-left my-8'>A web app for finding foodies with similar taste and dine together. Create groups by selecting your favorite restaurants and pick a date and time. Or join groups initated by others. Use the group chat to confirm details.</p>
+            <h4 className='text-slate-700 text-left text-2xl font-semibold my-2'>{textContent.projects.foodieClub.title}</h4>
+            <h5 className='text-sky-800 text-left italic'>{textContent.projects.foodieClub.subtitle}</h5>
+            <p className='text-slate-700 text-left my-8'>{textContent.projects.foodieClub.description}</p>
             {/* Tech Stack */}
-            <div className='flex flex-wrap justify-self-end text-xs'>
-              <span className='mx-2'>React</span>
-              <span className='mx-2'>FeathersJS</span>
-              <span className='mx-2'>MaterialUI</span>
-              <span className='mx-2'>PostgreSQL</span>
+            <div className='text-sky-800 flex flex-wrap justify-self-end text-xs'>
+              {textContent.projects.foodieClub.techStacks.map(tech => <span className='mx-2'>{tech}</span>)}
             </div>
           </div>
         </div>
-        {/* Project Block */}
-        <div className='my-16'>
-          <div className='relative w-full h-96 shadow-lg'>
+        {/* Project Block - FoodBag */}
+        <div className='my-16 flex md:justify-center md:items-center'>
+          <div className='order-2 relative w-full h-96 md:w-80 md:aspect-[1/2] md:mr-32 shadow-lg'>
             <img
               src={foodbag} 
               alt="Foodie Club Project Screenshot"
@@ -56,17 +51,25 @@ function ProjectsLine() {
             />
             {/* Text Block in Small Screen */}
             <div className='md:hidden absolute top-0 flex flex-col items-start p-6 w-full h-full bg-slate-900 opacity-90'>
-              <h4 className='text-white text-xl font-semibold my-2'>FoodBag (Ongoing Development)</h4>
-              <h5 className='text-white text-left italic'>A Platform for matching groups of foodies to dine together</h5>
-              <p className='text-white text-left my-8'>A web app for finding foodies with similar taste and dine together. Create groups by selecting your favorite restaurants and pick a date and time. Or join groups initated by others. Use the group chat to confirm details.</p>
+              <h4 className='text-white text-xl font-semibold my-2'>{textContent.projects.foodBag.title}</h4>
+              <h5 className='text-sky-500 text-left italic'>{textContent.projects.foodBag.subtitle}</h5>
+              <p className='text-white text-left my-8'>{textContent.projects.foodBag.description}</p>
               {/* Tech Stack */}
-              <div className='text-white flex flex-wrap justify-self-end text-xs'>
-                <span className='mx-2'>React Native</span>
-                <span className='mx-2'>AWS Amplify</span>
+              <div className='text-sky-500 flex flex-wrap justify-self-end text-xs'>
+                {textContent.projects.foodBag.techStacks.map(tech => <span className='mx-2'>{tech}</span>)}
               </div>
             </div>
           </div>
           {/* Text Block in Large Screen */}
+          <div className='order-1 hidden md:block w-96 p-6'>
+            <h4 className='text-slate-700 text-left text-2xl font-semibold my-2'>{textContent.projects.foodBag.title}</h4>
+            <h5 className='text-sky-800 text-left italic'>{textContent.projects.foodBag.subtitle}</h5>
+            <p className='text-slate-700 text-left my-8'>{textContent.projects.foodBag.description}</p>
+            {/* Tech Stack */}
+            <div className='text-sky-800 flex flex-wrap justify-self-end text-xs'>
+              {textContent.projects.foodBag.techStacks.map(tech => <span className='mx-2'>{tech}</span>)}
+            </div>
+          </div>
         </div>
       </div>
     </div>
---
+++ src/constants/textContent.js
@@ -0,0 +1,16 @@
+export const textContent = {
+  projects: {
+    foodieClub: {
+      title: 'Foodie Club',
+      subtitle: 'A Platform for matching groups of foodies to dine together',
+      description: 'A web app for finding foodies with similar taste and dine together. Create groups by selecting your favorite restaurants and pick a date and time. Or join groups initated by others. Use the group chat to confirm details.',
+      techStacks: ['React', 'FeathersJS', 'MaterialUI', 'PostgreSQL']
+    },
+    foodBag: {
+      title: 'FoodBag (Ongoing Development)',
+      subtitle: 'A Platform for matching groups of foodies to dine together',
+      description: 'A web app for finding foodies with similar taste and dine together. Create groups by selecting your favorite restaurants and pick a date and time. Or join groups initated by others. Use the group chat to confirm details.',
+      techStacks: ['React Native', 'AWS Amplify']
+    }
+  }
+}
