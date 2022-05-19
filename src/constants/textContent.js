84a35a3f9e58b34e2416ba9e2d15ed6f8e2b5f96
Matthew Lu <seedmachinegun@gmail.com>
2022-05-19 14:01:10 +0100

    Updated textContent

--- src/constants/textContent.js
+++ src/constants/textContent.js
@@ -2,14 +2,14 @@ export const textContent = {
   projects: {
     foodieClub: {
       title: 'Foodie Club',
-      subtitle: 'A Platform for matching groups of foodies to dine together',
+      subtitle: 'A platform for matching groups of foodies to dine together',
       description: 'A web app for finding foodies with similar taste and dine together. Create groups by selecting your favorite restaurants and pick a date and time. Or join groups initated by others. Use the group chat to confirm details.',
       techStacks: ['React', 'FeathersJS', 'MaterialUI', 'PostgreSQL']
     },
     foodBag: {
       title: 'FoodBag (Ongoing Development)',
-      subtitle: 'A Platform for matching groups of foodies to dine together',
-      description: 'A web app for finding foodies with similar taste and dine together. Create groups by selecting your favorite restaurants and pick a date and time. Or join groups initated by others. Use the group chat to confirm details.',
+      subtitle: 'Get great food at a cheaper price',
+      description: 'A mobile app for shoper owners, such as bakeries, to post food surplus of the day and sale them with discount. Users will get notified when there is food surplus available.',
       techStacks: ['React Native', 'AWS Amplify']
     }
   }
