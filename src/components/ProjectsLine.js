e8d0639100da3999613a0de4489d3062fa2fad3b
Matthew Lu <seedmachinegun@gmail.com>
2022-05-20 14:16:22 +0100

    adjust ProjectLines responsive layout

--- src/components/ProjectsLine.js
+++ src/components/ProjectsLine.js
@@ -22,22 +22,22 @@ function ProjectsLine() {
       {/* Project Blocks Container */}
       <div className='relative mt-12'>
         {/* Project Block - Foodie Club */}
-        <div className='my-16 flex items-center'>
-          <div ref={foodieClubRef} className={`relative w-full md:w-2/3 h-96 shadow-lg ${isFoodieClubVisible ? 'md:animate-fadein-from-l' : 'md:opacity-0'}`}>
+        <div className='my-16 flex md:justify-center md:items-center'>
+          <div ref={foodieClubRef} className={`relative w-full md:w-3/5 shadow-lg ${isFoodieClubVisible ? 'md:animate-fadein-from-l' : 'md:opacity-0'}`}>
             <ImgWithFallback
               src={foodieClubWebP}
               fallback={foodieClubPNG}
               alt='Foodie Club Project Screenshot'
-              className='w-full h-full object-cover object-left'
+              className='w-full h-full absolute md:static object-cover object-left'
             />
             {/* Text Block in Small Screen */}
-            <div className='md:hidden absolute top-0 flex flex-col items-start p-6 w-full h-full bg-slate-900 opacity-90'>
+            <div className='md:hidden flex flex-col items-start p-6 w-full h-full bg-slate-900 opacity-90'>
               <h4 className='text-white text-xl font-semibold my-2'>{textContent.projects.foodieClub.title}</h4>
               <h5 className='text-sky-500 text-left italic'>{textContent.projects.foodieClub.subtitle}</h5>
               <p className='text-white text-left my-8'>{textContent.projects.foodieClub.description}</p>
               {/* Tech Stack */}
               <div className='text-sky-500 flex flex-wrap justify-self-end text-xs'>
-                {textContent.projects.foodieClub.techStacks.map((tech, idx) => <span key={idx} className='mx-2'>{tech}</span>)}
+                {textContent.projects.foodieClub.techStacks.map((tech, idx) => <span key={idx} className='mx-2 my-1'>{tech}</span>)}
               </div>
               {/* Links - Github, Website */}
               <div className='flex items-center mt-6'>
@@ -57,7 +57,7 @@ function ProjectsLine() {
             <p className='text-slate-700 text-left my-8'>{textContent.projects.foodieClub.description}</p>
             {/* Tech Stack */}
             <div className='text-sky-800 flex flex-wrap justify-self-end text-xs'>
-              {textContent.projects.foodieClub.techStacks.map((tech, idx) => <span key={idx} className='mx-2'>{tech}</span>)}
+              {textContent.projects.foodieClub.techStacks.map((tech, idx) => <span key={idx} className='mx-2 my-1'>{tech}</span>)}
             </div>
             {/* Links - Github, Website */}
             <div className='flex items-center mt-6'>
@@ -76,7 +76,7 @@ function ProjectsLine() {
           <div
             ref={foodBagRef}
             className={
-              `order-2 relative w-full h-96 shadow-lg md:w-80 md:h-auto md:aspect-[5/11] md:mr-32
+              `order-2 relative w-full shadow-lg md:w-80 md:h-auto md:aspect-[5/11] md:mr-32
                md:border-transparent md:border-x-8 md:border-b-[16px] md:border-t-[60px] md:rounded-3xl
                md:box-border md:bg-cover md:bg-gradient-radial md:from-slate-800 md:via-slate-800 md:to-slate-600
                md:bg-clip-border before:md:contents-[""] before:md:w-16 before:md:h-1 before:md:bg-stone-500 before:md:absolute
@@ -87,16 +87,16 @@ function ProjectsLine() {
               src={foodbagWebm}
               alt='FoodBag Project Screenshot'
               fallback={foodbagPNG}
-              className='w-full h-full object-cover object-left md:rounded-xl'
+              className='w-full h-full absolute md:static object-cover object-left md:rounded-xl'
             />
             {/* Text Block in Small Screen */}
-            <div className='md:hidden absolute top-0 flex flex-col items-start p-6 w-full h-full bg-slate-900 opacity-90'>
+            <div className='md:hidden top-0 flex flex-col items-start p-6 w-full h-full bg-slate-900 opacity-90'>
               <h4 className='text-white text-xl font-semibold my-2'>{textContent.projects.foodBag.title}</h4>
               <h5 className='text-sky-500 text-left italic'>{textContent.projects.foodBag.subtitle}</h5>
               <p className='text-white text-left my-8'>{textContent.projects.foodBag.description}</p>
               {/* Tech Stack */}
               <div className='text-sky-500 flex flex-wrap justify-self-end text-xs'>
-                {textContent.projects.foodBag.techStacks.map((tech, idx) => <span key={idx} className='mx-2'>{tech}</span>)}
+                {textContent.projects.foodBag.techStacks.map((tech, idx) => <span key={idx} className='mx-2 my-1'>{tech}</span>)}
               </div>
             </div>
           </div>
@@ -107,7 +107,7 @@ function ProjectsLine() {
             <p className='text-slate-700 text-left my-8'>{textContent.projects.foodBag.description}</p>
             {/* Tech Stack */}
             <div className='text-sky-800 flex flex-wrap justify-self-end text-xs'>
-              {textContent.projects.foodBag.techStacks.map((tech, idx) => <span key={idx} className='mx-2'>{tech}</span>)}
+              {textContent.projects.foodBag.techStacks.map((tech, idx) => <span key={idx} className='mx-2 my-1'>{tech}</span>)}
             </div>
           </div>
         </div>
