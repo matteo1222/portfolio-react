91e61184c26d6cec4314be27253a44337a70b77f
Matthew Lu <seedmachinegun@gmail.com>
2022-07-28 12:48:55 +0100

    Updating Contact Section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,23 +1,50 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 function Contact() {
-  const EMAIL = 'matthewlu.tw@gmail.com'
-  const [copied, setCopied] = useState(false)
+  const EMAIL = "matthewlu.tw@gmail.com";
+  const [copied, setCopied] = useState(false);
 
   const handleClick = () => {
-    setCopied(true)
-    navigator.clipboard.writeText(EMAIL)
+    setCopied(true);
+    navigator.clipboard.writeText(EMAIL);
     setTimeout(() => {
-      setCopied(false)
-    }, 1000)
-  }
+      setCopied(false);
+    }, 1000);
+  };
 
   return (
-    <address id='contact' className='mt-8 h-96 flex flex-col items-center justify-center'>
-      <button onClick={handleClick} className='text-4xl hover:cursor-pointer'>Contact</button>
-      <p className={`origin-bottom opacity-0 ${copied ? 'animate-rotate-in-and-out' : ''}`}>Email Copied!</p>
-    </address>
-  )
+    <section
+      id="contact"
+      className="mt-8 h-96 flex flex-col items-center justify-center"
+    >
+      <h3 className="text-4xl">Get In Touch</h3>
+      {/* Spacing */}
+      <div className="p-2"></div>
+      <p className="max-w-xl">
+        I am currently looking for Front-End Developer role. Drop me a message
+        and let's start from there!
+      </p>
+      {/* Spacing */}
+      <div className="p-8"></div>
+      <address>
+        <a
+          href="mailto:matthewlu.tw@gmail.com"
+          rel="noopener noreferrer"
+          target="_blank"
+          className="bg-black text-white py-3 px-6 rounded hover:cursor-pointer hover:opacity-75"
+        >
+          Say Hi
+        </a>
+        {/* <p
+          className={`origin-bottom opacity-0 ${
+            copied ? "animate-rotate-in-and-out" : ""
+          }`}
+        >
+          Email Copied!
+        </p> */}
+      </address>
+    </section>
+  );
 }
 
-export default Contact
+export default Contact;
